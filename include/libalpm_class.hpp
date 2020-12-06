baaf6d3d9de916a9b9302c6ca2eaf433ae14c522
kokkiemouse <kokkiemouse@fascode.net>
2020-12-06 23:45:34 +0900

    added
    Signed-off-by: kokkiemouse <kokkiemouse@fascode.net>

--- include/libalpm_class.hpp
+++ include/libalpm_class.hpp
@@ -1,11 +1,12 @@
 #pragma once
 #include <alpm.h>
 #include <string>
-#define String  std::string
+typedef std::string String;
 class alpm_class{
     public:
         alpm_class(String root,String dbpath);
         ~alpm_class();
+        alpm_db_t* register_syncdb(String treename,int level);
     private:
         alpm_handle_t* handle;
 };
--- src/libalpm_class.cpp
+++ src/libalpm_class.cpp
@@ -7,3 +7,6 @@ alpm_class::alpm_class(String root,String dbpath){
 alpm_class::~alpm_class(){
     alpm_release(handle);
 }
+alpm_db_t* alpm_class::register_syncdb(String treename,int level){
+    return alpm_register_syncdb(handle,treename.c_str(),level);
+}
