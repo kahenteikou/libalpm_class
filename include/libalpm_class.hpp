9814e3cdb01bf4dd4bd182ecf075ae2baea4e476
kokkiemouse <kokkiemouse@fascode.net>
2020-12-07 07:14:31 +0900

    added
    Signed-off-by: kokkiemouse <kokkiemouse@fascode.net>

--- include/libalpm_class.hpp
+++ include/libalpm_class.hpp
@@ -1,12 +1,20 @@
 #pragma once
 #include <alpm.h>
 #include <string>
+#include <vector>
 typedef std::string String;
+#define Vector  std::vector
 class alpm_class{
     public:
         alpm_class(String root,String dbpath);
         ~alpm_class();
         alpm_db_t* register_syncdb(String treename,int level);
+        alpm_db_t* get_localdb();
+        Vector<alpm_db_t*> get_syncdbs();
     private:
         alpm_handle_t* handle;
+        template<class T> 
+        Vector<T> to_array_pointer(alpm_list_t* lskun); 
+        template<class T>
+        void to_array_p(alpm_list_t* lskun,Vector<T>* vectp);
 };
--- src/libalpm_class.cpp
+++ src/libalpm_class.cpp
@@ -10,3 +10,25 @@ alpm_class::~alpm_class(){
 alpm_db_t* alpm_class::register_syncdb(String treename,int level){
     return alpm_register_syncdb(handle,treename.c_str(),level);
 }
+alpm_db_t* alpm_class::get_localdb(){
+    return alpm_get_localdb(handle);
+}
+Vector<alpm_db_t*> alpm_class::get_syncdbs(){
+    return to_array_pointer<alpm_db_t*>(alpm_get_syncdbs(handle));
+}
+
+template<class T> 
+Vector<T> alpm_class::to_array_pointer(alpm_list_t* lskun){
+    Vector<T> return_v_kun;
+    to_array_p(lskun,&return_v_kun);
+    return return_v_kun;
+}
+template<class T>
+void alpm_class::to_array_p(alpm_list_t* lskun,Vector<T>* vectp){
+    if(lskun->data != nullptr){
+        vectp->push_back((T)lskun->data);
+    }
+    if(lskun->next !=nullptr){
+        to_array_p((__alpm_list_t*)lskun->next,vectp);
+    }
+}
