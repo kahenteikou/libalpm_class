78a008f725ba8672b51d44a01a26f503ac54ea17
kokkiemouse <kokkiemouse@fascode.net>
2020-12-21 16:56:22 +0900

    ??
    Signed-off-by: kokkiemouse <kokkiemouse@fascode.net>

--- include/alpm_pkg_class.hpp
+++ include/alpm_pkg_class.hpp
@@ -1,12 +1,20 @@
 #pragma once
 #include <alpm.h>
 #include "alpm_pkg_ver.hpp"
+#include <vector>
+#include <string>
+
 class alpm_pkg_class{
     public:
         alpm_pkg_class(alpm_pkg_t* pkgkun);
         ~alpm_pkg_class();
         alpm_pkg_ver get_version();
+        std::vector<alpm_pkg_class> get_depends_packages();
         operator alpm_pkg_t*();
     private:
         alpm_pkg_t* pkgptr;
+        template<class T> 
+        std::vector<T> to_array_pointer(alpm_list_t* lskun); 
+        template<class T>
+        void to_array_p(alpm_list_t* lskun,std::vector<T>* vectp);
 };
--- src/alpm_pkg_class.cpp
+++ src/alpm_pkg_class.cpp
@@ -1,4 +1,8 @@
 #include "alpm_pkg_class.hpp"
+
+typedef std::string String;
+template<typename T>
+using Vector=std::vector<T>;
 alpm_pkg_class::alpm_pkg_class(alpm_pkg_t* pkgkun){
     pkgptr=pkgkun;
 }
@@ -10,4 +14,23 @@ alpm_pkg_class::operator alpm_pkg_t*(){
 }
 alpm_pkg_ver alpm_pkg_class::get_version(){
     return alpm_pkg_ver(alpm_pkg_get_version(pkgptr));
+}
+
+Vector<alpm_pkg_class> alpm_pkg_class::get_depends_packages(){
+
+}
+template<class T> 
+Vector<T> alpm_pkg_class::to_array_pointer(alpm_list_t* lskun){
+    Vector<T> return_v_kun;
+    to_array_p(lskun,&return_v_kun);
+    return return_v_kun;
+}
+template<class T>
+void alpm_pkg_class::to_array_p(alpm_list_t* lskun,Vector<T>* vectp){
+    if(lskun->data != nullptr){
+        vectp->push_back((T)lskun->data);
+    }
+    if(lskun->next !=nullptr){
+        to_array_p((__alpm_list_t*)lskun->next,vectp);
+    }
 }
